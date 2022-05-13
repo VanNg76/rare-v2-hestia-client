a751ddbd79e9c45873f77b00ddc4768ceee70c87
Kevin Caruso <Kjcaruso12@gmail.com>
2022-05-13 14:17:10 -0500

    Updated CategoryManager and CategoryForm

--- src/components/categories/CreateCategoryForm.js
+++ src/components/categories/CreateCategoryForm.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { fetchIt } from "../utils/Fetch"
 import { Settings } from "../utils/Settings"
+import { createCategory } from "./CategoryManager";
 
 // def a function that will return a new category form
 
@@ -19,24 +20,24 @@ export const NewCategoryForm = ({ getCategories }) => {
     // e.preventDefault()
         // defines a new  variable which will be an object for the new category, "newCategory"
         // the object will have one key value pair:
-            // label: form.category 
+            // label: form.category
                 // define a new variable, fetchOption, method will be POST, headers will be "Content-Type": "application/json"
                 // convert what we're sending to the server into json body: JSON.stringify(newCategory)
-        
+
                 const submitNewCategory = (e) => {
                     e.preventDefault()
                     const newCategory = {
                         label: form.label,
                     }
-                    return fetchIt(`${Settings.API}/categories`, "POST", JSON.stringify(newCategory))
+                    createCategory(newCategory)
                             .then(getCategories)
                 }
 
         // post the newCategory to the Categories table in the db
-        // return fetch("http://localhost:8088/categories", fetchOption) 
-        
+        // return fetch("http://localhost:8088/categories", fetchOption)
+
         // example:
-        
+
         // const submitCategory = (e) => {
         //     e.preventDefault()
         //     const newCategory = {
@@ -49,13 +50,13 @@ export const NewCategoryForm = ({ getCategories }) => {
         //         },
         //         body: JSON.stringify(newCategory)
         //     }
-        
+
         //     return fetch("http://localhost:8088/categories", fetchOption)
         // }
 
 
 
-// return 
+// return
     // wrap in div className "form-group"
         // <label htmlFor="category" "create a new category" as text for label
         // input category
@@ -64,12 +65,12 @@ export const NewCategoryForm = ({ getCategories }) => {
             // className="form-control"
             // placeholder="add text"
                 // add an onChange function to update what we will send to the server as the user types
-                    // accepts a parameter "e" 
+                    // accepts a parameter "e"
                     //  => function body:
                         // defines a new variable, copy, which is equal to { ...form}
                         // set copy.label equal to e.target.value
                         // change the value of form by using updateForm and passing in copy as an argument
-                        
+
                         // example:
                         return (
                             <>
@@ -91,7 +92,7 @@ export const NewCategoryForm = ({ getCategories }) => {
                                             }
                                         />
                                         <div className="submitButtonCreateNewCategoryForm">
-                    
+
                                             <button onClick={(e) => {
                                                 submitNewCategory(e)
                                                 updateForm({label: ""})
@@ -105,7 +106,7 @@ export const NewCategoryForm = ({ getCategories }) => {
                         )
                                         }
 
-                        
+
 // add a button, which when clicked will invoke the submit new category function from the top of this module
 /* <button onClick={submitNewCategory} className="submit-button">
                     Submit
