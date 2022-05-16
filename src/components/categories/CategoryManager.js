bd99f8cb4f3e8471d3fbd2eacc3100109a14d034
Kevin Caruso <Kjcaruso12@gmail.com>
2022-05-16 11:32:51 -0500

    Updated CreateCategoryForm to allow for editing and updating ApplicationViews to direct to an edit page and back to categories list

--- src/components/categories/CategoryManager.js
+++ src/components/categories/CategoryManager.js
@@ -6,8 +6,16 @@ export const getAllCategories = () => {
   return fetchIt(`${Settings.API}/categories`)
 }
 
+export const getSingleCategory = (categoryId) => {
+  return fetchIt(`${Settings.API}/categories/${categoryId}`)
+}
+
 export const createCategory = (newCategory) => {
   return fetchIt(`${Settings.API}/categories`, "POST", JSON.stringify(newCategory))
+}
+
+export const updateCategory = (newCategory) => {
+  return fetchIt(`${Settings.API}/categories/${newCategory.id}`, "PUT", JSON.stringify(newCategory))
 }
 
 export const deleteCategory = (categoryId) => {
